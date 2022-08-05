c393d8a8e5f52ac486750267b1baa3986f7cca62
정혜지 <93106858+my8za@users.noreply.github.com>
2022-08-05 22:01:31 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,10 +2,10 @@ const btns = document.querySelectorAll(".btns li");
 const boxs = document.querySelectorAll("section article");
 
 
-for(let i =0; i<btns.length; i++) {
-   for(let i=0; i<btns.length; i++){
-    btns[i].addEventListener("click", () => {
-        for(let j=0; j<btns.length; j++) {
+for(let i = 0; i < btns.length; i++) {
+   for(let i = 0; i < btns.length; i++){
+    btns[i].addEventListener ("click", () => {
+        for (let j=0; j<btns.length; j++) {
             btns[j].classList.remove('on');
             boxs[j].classList.remove('on');
         }
