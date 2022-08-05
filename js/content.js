8ba95da24ef1f16422290f881a56f1a71e5c042e
정혜지 <93106858+my8za@users.noreply.github.com>
2022-08-05 23:07:53 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,17 +1,18 @@
 const btns = document.querySelectorAll(".btns li");
 const boxs = document.querySelectorAll("section article");
 
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener('click', () => {
+    for (let j = 0; j < btns.length; j++) {
+      btns[j].classList.remove('on');
+      boxs[j].classList.remove('on');
+    }
 
-for(let i = 0; i < btns.length; i++) {
-   for(let i = 0; i < btns.length; i++){
-    btns[i].addEventListener ("click", () => {
-        for (let j=0; j<btns.length; j++) {
-            btns[j].classList.remove('on');
-            boxs[j].classList.remove('on');
-        }
-        btns[i].classList.add('on');
-        boxs[i].classList.add('on');
-    });
-};
+    btns[i].classList.add('on');
+    boxs[i].classList.add('on');
 
+
+  });
+
+}
 
