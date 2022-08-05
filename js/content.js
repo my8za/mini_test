f1b601bcdd074bd91336c02658ede47ffaa84c83
정혜지 <93106858+my8za@users.noreply.github.com>
2022-08-05 21:59:55 +0900

    Update content.js

--- js/content.js
+++ js/content.js
@@ -2,19 +2,16 @@ const btns = document.querySelectorAll(".btns li");
 const boxs = document.querySelectorAll("section article");
 
 
-// 버튼의 개수만큼 반복, 해당 요소 이벤트 등록
-
 for(let i =0; i<btns.length; i++) {
-    btns[i].addEventListener('click', () => {
-        boxs[i].classList.add('on')
-        btns[i].classList.add('on')
-        
-    });
-    btns[i].addEventListener('mouseout', () => {
-        boxs[i].classList.remove('on')
-        btns[i].classList.remove('on')
+   for(let i=0; i<btns.length; i++){
+    btns[i].addEventListener("click", () => {
+        for(let j=0; j<btns.length; j++) {
+            btns[j].classList.remove('on');
+            boxs[j].classList.remove('on');
+        }
+        btns[i].classList.add('on');
+        boxs[i].classList.add('on');
     });
 };
-
 
 
